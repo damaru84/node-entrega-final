b0af00fcad5b0bdf9914b99e537bf891a421c08a
damaru84 <soydanielito84@gmail.com>
2026-07-11 01:09:43 -0300

    arreglos

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -3,15 +3,11 @@ import { createProduct, getAllProducts, getProductById, updateProduct, deletePro
 import { authentication } from '../middleware/authentication.js';
 const router = express.Router();
 
-router.get('products', getAllProducts);
-
-router.get("products/:id", getProductById);
-
-router.post("products/create", authentication, createProduct);
-
-router.put("products/:id", authentication, updateProduct);
-
-router.delete("products/:id", authentication, deleteProduct)
+router.get('/products', getAllProducts);
+router.get('/products/:id', getProductById);
+router.post('/products/create', authentication, createProduct);
+router.put('/products/:id', authentication, updateProduct);
+router.delete('/products/:id', authentication, deleteProduct);
 
 //router.get("")
 
